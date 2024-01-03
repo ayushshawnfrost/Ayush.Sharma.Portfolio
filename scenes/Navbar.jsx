9a9397d5d3bcc1f947c2148c2fdedc7c80a4656d
Ayush <ayushmarkevens@gmail.com>
2024-01-03 00:51:09 -0800

    Updates

---
+++ scenes/Navbar.jsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+import close from "../assets/close-icon.svg";
+import menu from "../assets/menu-icon.svg";
+import { Link as Route } from "react-router-dom";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      } hover:text-yellow transition duration-500 font-playfair`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-red";
+
+  return (
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold"></h4>
+
+        {/* DESKTOP NAV */}
+        {isDesktop ? (
+          <div className="flex justify-between gap-16 font-opensans text-l font-semibold">
+            <Link
+              page="About"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Blog"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Experience"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            {/* <Route
+              className={`hover:text-yellow transition duration-500 font-playfair`}
+              to="blogs"
+            >
+              Login
+            </Route> */}
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-red p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img alt="menu-icon" src={menu} />
+          </button>
+        )}
+
+        {/* MOBILE MENU POPUP */}
+        {!isDesktop && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img alt="close-icon" src={close} />
+              </button>
+            </div>
+
+            {/* MENU ITEMS */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link
+                page="About"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Blog"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Experience"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
